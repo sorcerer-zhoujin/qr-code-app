a49d1da7a8ff232f2aceda4bd53723940758b782
Zhou Jin <zhoujin@sorcerer.co.jp>
2023-07-20 10:31:36 +0900

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState, useEffect } from "react";
+import QRCode from "react-qr-code";
+//import QrReader from "react-qr-scanner"
+import { useZxing } from "react-zxing";
 import './App.css';
 
-function App() {
+export default function App() {
+  const [inputText, setInputText] = useState("Hello World!");
+  const [isCameraActive, setCamera] = useState(false);
+  const [cameraButtonText, setCameraButtonText] = useState("OFF");
+  const [result, setResult] = useState("Scanning...");
+
+  useEffect(() => {
+    if (result === "" || result === "Scanning...") return;
+    alert(result);
+  }, [result]);
+
+  const { ref } = useZxing({
+    onResult(result) {
+      setResult(result.getText());
+    },
+    paused: !isCameraActive,
+  });
+
+  const handleInputChange = (e: any) => {
+    setInputText(e.target.value);
+  };
+
+  const handleScanButtonClick = () => {
+    setCamera(!isCameraActive);
+    setCameraButtonText(!isCameraActive ? "ON" : "OFF");
+    setResult("Scanning...");
+  };
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <>
+      <div style={{ height: "auto", margin: "0 auto", maxWidth: 256, width: "100%" }}>
+        <QRCode
+          size={256}
+          style={{ height: "auto", maxWidth: "100%", width: "100%" }}
+          value={inputText}
+          viewBox={`0 0 256 256`}
+        />
+        <input
+          type="text"
+          value={inputText}
+          onChange={handleInputChange}
+          style={{ width: '100%' }}
+        />
+      </div>
+      <button onClick={handleScanButtonClick}>
+        Camera: {cameraButtonText}
+      </button>
+      <div hidden={!isCameraActive}>
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          <span>RESULT: </span>
+          <span>{result}</span>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <video ref={ref} />
+      </div>
+    </>
   );
 }
-
-export default App;
